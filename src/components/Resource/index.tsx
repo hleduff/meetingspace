7770b1f8e94e23bd7b1404083d66b28bbfd2930f
Hugo Le Duff <hleduff@yahoo.com>
2023-01-05 21:23:20 +0100

    feat: style Resource component

--- src/components/Resource/index.tsx
+++ src/components/Resource/index.tsx
@@ -1,13 +1,14 @@
 import { useGetResourceQuery } from '../../features/api/apiSlice';
 
+import styles from './style.module.css';
+
 // displays the available room
 export const Resource = () => {
     const { data: resource } = useGetResourceQuery();
 
     return (
-        <p className="regText">
-            You're currently availability for room:&nbsp;
-            <b className="highlight">{resource?.data.name}</b>
+        <p className={styles.root}>
+            Current room: <b className="highlight">{resource?.data.name}</b>
         </p>
     );
 };
