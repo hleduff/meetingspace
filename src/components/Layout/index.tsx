bfca4a8cf73166164662896a0c4de0fe6c4870d6
Hugo Le Duff <hleduff@yahoo.com>
2023-01-06 18:47:08 +0100

    fix(minor): import syntax

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import { ReactNode } from 'react';
 
 import { Header } from '../Header';
 
 import styles from './style.module.css';
 
-export const Layout = ({ children }: { children: React.ReactNode }) => (
+export const Layout = ({ children }: { children: ReactNode }) => (
     <div className={styles.layout}>
         <Header />
         <main className={styles.main}>{children}</main>
