75ec2af1a06887e75ce2d31ef92fc57ab359149b
Hugo Le Duff <hleduff@yahoo.com>
2023-01-08 02:28:11 +0100

    feat: mobile menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,6 @@
+import { useState } from 'react';
 import { useDispatch } from 'react-redux';
+import classnames from 'classnames';
 
 import { useAppSelector } from '../../app/store';
 import { useGetMeMutation, useLoginMutation, useLogoutMutation, useResetEnvironmentMutation } from '../../features/api/apiSlice';
@@ -10,6 +12,7 @@ import styles from './style.module.css';
 
 export const Header = () => {
     const dispatch = useDispatch();
+    const [menuOpen, setMenuOpen] = useState(false);
 
     const [login, { isLoading: loginLoading }] = useLoginMutation();
     const [getMe] = useGetMeMutation();
@@ -47,44 +50,80 @@ export const Header = () => {
         }
     };
 
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
     return (
         <header className={styles.header}>
             <h1 className={styles.title}>
                 meeting<span className="highlight">space</span>
             </h1>
-            <div className={styles.menu}>
-                {!authToken &&
-                    <button
-                        className="btn"
-                        disabled={loginLoading}
-                        onClick={handleLogin}
-                        type="button"
-                    >
-                        Login
-                    </button>
-                }
-                {userName && (
-                    <>
-                        <div className={styles.user}>
-                            {userName}
+            <button
+                aria-controls="siteMenu"
+                aria-haspopup="true"
+                aria-label="Toggle site menu"
+                className={styles.menuButton}
+                onClick={toggleMenu}
+                type="button"
+            >
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" className="bi" fill="currentColor" viewBox="0 0 16 16">
+                    <path fillRule="evenodd" d="M2.5 11.5A.5.5 0 0 1 3 11h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 7h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 3h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"></path>
+                </svg>
+            </button>
+            <div
+                aria-label="Site menu"
+                className={classnames(styles.menuWrapper, menuOpen && styles.open)}
+                id="siteMenu"
+                tabIndex={-1}
+                {...(menuOpen && {role: "dialog", "aria-modal" : "true"})}
+            >
+                <div className={styles.menuContainer}>
+                    <div className={styles.menu}>
+                        <div className={styles.menuHeader}>
+                            {userName && (
+                                <div className={styles.user}>
+                                    {userName}
+                                </div>
+                            )}
+                            <button
+                                aria-label="Close"
+                                className={styles.closeButton}
+                                onClick={toggleMenu}
+                                type="button"
+                            >
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path fill="#f9f9f9" fillRule="evenodd" d="M5.47 5.47a.75.75 0 0 1 1.06 0l12 12a.75.75 0 1 1-1.06 1.06l-12-12a.75.75 0 0 1 0-1.06Z" clipRule="evenodd"/><path fill="#f9f9f9" fillRule="evenodd" d="M18.53 5.47a.75.75 0 0 1 0 1.06l-12 12a.75.75 0 0 1-1.06-1.06l12-12a.75.75 0 0 1 1.06 0Z" clipRule="evenodd"/></svg>
+                            </button>
                         </div>
+                        {!authToken &&
+                            <button
+                                className="btn"
+                                disabled={loginLoading}
+                                onClick={handleLogin}
+                                type="button"
+                            >
+                                Login
+                            </button>
+                        }
+                        {userName && (
+                            <button
+                                className="btn"
+                                disabled={logoutLoading}
+                                onClick={handleLogout}
+                                type="button"
+                            >
+                                Logout
+                            </button>
+                        )}
                         <button
                             className="btn"
-                            disabled={logoutLoading}
-                            onClick={handleLogout}
+                            onClick={handleReset}
                             type="button"
                         >
-                            Logout
+                            Reset
                         </button>
-                    </>
-                )}
-                <button
-                    className="btn"
-                    onClick={handleReset}
-                    type="button"
-                >
-                    Reset
-                </button>
+                    </div>
+                </div>
             </div>
         </header>
     );
